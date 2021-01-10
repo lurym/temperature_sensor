d5e141fdc730ce947b8b22f2d3870bcc8ab74ff8
lurym <lurym89@gmail.com>
2021-01-10 18:20:25 +0100

    Improved version, update to python 3

---
+++ config.py
@@ -0,0 +1,2 @@
+write_api_key = "XXXXXXXXXXXXXXXX"
+channel_id = "1111111"
--- temp.py
+++ temp.py
@@ -1,86 +1,70 @@
-import urllib2 as ul
+import requests
 import json
 import time
 import os
 from w1thermsensor import W1ThermSensor
 import logging
+import config
 
-lastConnectionTime = time.time() # Track the last connection time
-lastUpdateTime = time.time() # Track the last update time
-postingInterval = 120 # Post data once every 2 minutes
-updateInterval = 60 # Update once every 15 seconds
+posting_interval = 120	# Post data once every 2 minutes
+update_interval = 60	# Update once every 60 seconds
 
-writeAPIkey = "API_KEY" # Replace YOUR-CHANNEL-WRITEAPIKEY with your channel write API key
-channelID = "CHANNEL_ID" # Replace YOUR-CHANNELID with your channel ID
-url = "https://api.thingspeak.com/channels/"+channelID+"/bulk_update.json" # ThingSpeak server settings
-messageBuffer = [] # Buffer to hold the data
+url = "https://api.thingspeak.com/channels/"+config.channel_id+"/bulk_update.json" # ThingSpeak server settings
 
 logging.basicConfig(level=logging.DEBUG)
 
-def httpRequest():
+def http_request(message_buffer):
     '''Function to send the POST request to 
     ThingSpeak channel for bulk update.'''
 
-    global messageBuffer
-    logging.info("httpRequest called")
-    data = json.dumps({'write_api_key':writeAPIkey,'updates':messageBuffer}) # Format the json data buffer
-    req = ul.Request(url = url)
-    requestHeaders = {"User-Agent":"mw.doc.bulk-update (Raspberry Pi)","Content-Type":"application/json","Content-Length":str(len(data))}
-    for key,val in requestHeaders.iteritems(): # Set the headers
-        req.add_header(key,val)
-    req.add_data(data) # Add the data to the request
-    # Make the request to ThingSpeak
-    try:
-        response = ul.urlopen(req) # Make the request
-        logging.info(response.getcode()) # A 202 indicates that the server has accepted the request
-    except ul.HTTPError as e:
-        logging.info(e.code) # Print the error code
-    messageBuffer = [] # Reinitialize the message buffer
-    global lastConnectionTime
-    lastConnectionTime = time.time() # Update the connection time
+    logging.info("http_request called")
+    data = json.dumps({'write_api_key':config.write_api_key,'updates':message_buffer})
+    request_headers = {"User-Agent":"mw.doc.bulk-update (Raspberry Pi)", "Content-Type":"application/json","Content-Length":str(len(data))}
+    response = requests.post(url, data, headers=request_headers)
+    logging.info("status code of the response " + str(response.status_code))
+
+    message_buffer.clear()
 
 def getData():
-    '''Function that returns the CPU temperature and percentage of CPU utilization'''
-    sensor_outside = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "00000550ff55")
-    temp_outside = sensor_outside.get_temperature()
-    logging.info("Temperature outside " + str(temp_outside))
-    sensor_out = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "000005512327")
-    temp_out = sensor_out.get_temperature()
-    logging.info("Temperature water out " + str(temp_out))
-    sensor_in = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0000054ff75c")
-    temp_in = sensor_in.get_temperature()
-    logging.info("Temperature water in " + str(temp_in))
-    return temp_outside, temp_in, temp_out
+    '''Function that returns the data about sensors'''
+    results = []
+    for sensor in W1ThermSensor.get_available_sensors():
+        temperature = sensor.get_temperature()
+        id = sensor.id
+        temperature_string = str(temperature)
+        logging.info("Temperature " + id + " value " + temperature_string)
+        results.append(temperature)
+    return results
 
-def updatesJson():
+def update_json(last_update_time, message_buffer):
     '''Function to update the message buffer
-    every 15 seconds with data. And then call the httpRequest 
-    function every 2 minutes. This examples uses the relative timestamp as it uses the "delta_t" parameter. 
-    If your device has a real-time clock, you can also provide the absolute timestamp using the "created_at" parameter.
+    every 60 seconds with data. 
     '''
 
-    global lastUpdateTime
-    logging.info("updatesJson called")
+    logging.info("update_json called")
     message = {}
-    message['delta_t'] = time.time() - lastUpdateTime
-    outside, temp_in, temp_out = getData()
-    message['field1'] = outside
-    message['field2'] = temp_in
-    message['field3'] = temp_out
-    global messageBuffer
-    messageBuffer.append(message)
-    # If posting interval time has crossed 2 minutes update the ThingSpeak channel with your data
-    if time.time() - lastConnectionTime >= postingInterval:
-        httpRequest()
-    lastUpdateTime = time.time()
+    message['delta_t'] = str(int(round(time.time() - last_update_time)))
+    temperatures = getData()
+    for i, temp in enumerate(temperatures):
+    	message['field' + str(i+1)] = temp
+    message_buffer.append(message)
 
 if __name__ == "__main__":  # To ensure that this is run directly and does not run when imported 
     logging.info("Starting temperature measure main loop")
+    message_buffer 		= []
+    last_update_time 		= time.time()
+    last_connection_time 	= time.time()
+    update_json(last_update_time, message_buffer)
+    http_request(message_buffer)
     while 1:
         try:
-            getData()
-            if time.time() - lastUpdateTime >= updateInterval:
-                updatesJson()
+            if time.time() - last_update_time >= update_interval:
+                update_json(last_update_time, message_buffer)
+                last_update_time = time.time()
+	    # If posting interval time has crossed 2 minutes update the ThingSpeak channel with your data
+            if time.time() - last_connection_time >= posting_interval:
+                http_request(message_buffer)
+                last_connection_time = time.time()
             time.sleep(5)
         except Exception as e:
             logging.exception("Exception in main loop")
